d639ea8c2c60cd26319d85e410fac4b6858900e3
haroonbaloch <suwinepenz@gmail.com>
2018-09-10 12:59:34 -0700

    s

--- mytestproject/app2/views.py
+++ mytestproject/app2/views.py
@@ -7,5 +7,5 @@ def index2(anyname):
     return HttpResponse("my 2nd rpoject")
 
 def index3(request):
-    dict = {'templatetag':"your teasxfv here"}
+    dict = {'templatetag':"your haroon here"}
     return render(request,'html/index.html',context=dict)
