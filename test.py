359bddc6b6f3a2b8f464a6f64133882b174951bd
haroonbaloch <suwinepenz@gmail.com>
2018-09-10 23:58:17 +0500

    dfg

---
+++ test.py
@@ -0,0 +1,16 @@
+class honda():
+    wheels="four wheels"
+    def __init__(self, name, color, gear=5):
+        self.name=name
+        self.color=color
+        self.gear=gear
+    def speed():
+        self.gear*20
+        
+
+car=honda("civic","blue")
+
+print (car.color)
+print (car.name)
+print (car.speed)
+print (car.gear)
